f49c61217fbea89c64a9ccebc4ba1d3bf2770a6e
fnusid <sidcs@umich.edu>
2026-01-20 17:53:22 +0000

    Adding eval metrics tsne

--- eval_metrics.py
+++ eval_metrics.py
@@ -314,8 +314,7 @@ def plot_tsne_subset(embs, labels, num_speakers=4, save_path="tsne_subset.png"):
         n_components=2,
         perplexity=20,
         learning_rate="auto",
-        init="pca",
-        random_state=0,
+        init="pca"
     )
     coords = tsne.fit_transform(X)
 
@@ -338,10 +337,10 @@ def plot_tsne_subset(embs, labels, num_speakers=4, save_path="tsne_subset.png"):
 if __name__ == "__main__":
     META = "/mnt/disks/data/datasets/Datasets/LibriMix/LibriMix/Libriuni_05_08/Libri2Mix_ovl50to80/wav16k/min/metadata/mixture_test_mix_clean.csv"
     CKPT = "/mnt/disks/data/model_ckpts/librispeech_asp_ft_wavlm_linear_dualemb_tr360/best-epoch=49-val_separation=0.000.ckpt"
-    ckpt_joint_trained = "/mnt/disks/data/model_ckpts/pDCCRN_2sp_dpccn_joint_training_freezewavlm_indloss/best-epoch=11-val_separation=0.000.ckpt"
+    ckpt_joint_trained = "/mnt/disks/data/model_ckpts/pDCCRN_2sp_dpccn_joint_training_freezewavlm_indloss/best-epoch=19-val_separation=0.000.ckpt"
     # CKPT = "/mnt/disks/data/model_ckpts/librispeech_asp_wavlm_dualemb/best-epoch=50-val_separation=0.000.ckpt" # WITHOUT FINE-TUNING WAVLM LAST 6 LAYERS
     TEACHER_CKPT = "/mnt/disks/data/model_ckpts/librispeech_asp_wavlm_tr360/best-epoch=62-val_separation=0.000.ckpt"
-    TSNE_SAVE_PATH = "/home/sidharth./codebase/wavlm_dual_embedding/analysis/tsne_new/dual_devclean_whamtt_subset_tsne_linearasp.png"
+    TSNE_SAVE_PATH = "/home/sidharth./codebase/wavlm_dual_embedding/analysis/tsne_final/two_sp_test_joint_train_seed42.png"
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Using device:", device)
@@ -404,4 +403,4 @@ if __name__ == "__main__":
     print(f"silhouette    = {res['silhouette']:.4f}")
 
     # ---- TSNE on subset of speakers ----
-    # plot_tsne_subset(embs, labels, num_speakers=4, save_path=TSNE_SAVE_PATH)
+    plot_tsne_subset(embs, labels, num_speakers=4, save_path=TSNE_SAVE_PATH)
